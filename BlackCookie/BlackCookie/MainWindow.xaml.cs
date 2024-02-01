2f8b1469529ee82c47fa98b278a68a35a9d0b72c
basloc <bastienlocat@gmail.com>
2024-02-01 18:00:54 +0100

    random et autoclick fait

--- BlackCookie/BlackCookie/MainWindow.xaml.cs
+++ BlackCookie/BlackCookie/MainWindow.xaml.cs
@@ -1,6 +1,9 @@
 ﻿using System.Windows;
 using Newtonsoft.Json;
 using System.IO;
+using System;
+using System.Timers;
+using System.Windows.Threading;
 
 
 namespace BlackCookie
@@ -12,6 +15,10 @@ namespace BlackCookie
     {
         public int points = 0;
         int cookieGains = 1;
+        private Random random = new Random();
+        private Timer autoClickerTimer = new Timer();
+        private Dispatcher uiDispatcher;
+        
 
         private static MainWindow? instance;
 
@@ -30,6 +37,10 @@ namespace BlackCookie
         {
             InitializeComponent();
             LoadGame();
+            uiDispatcher = Dispatcher.CurrentDispatcher;
+            autoClickerTimer.Interval = 1000; // 1000 millisecondes (1 seconde)
+            autoClickerTimer.Elapsed += AutoClickerTimerElapsed;
+            autoClickerTimer.Start();
             Closing += MainWindow_Closing;
 
         }
@@ -37,7 +48,8 @@ namespace BlackCookie
         private void Point_onclick(object sender, RoutedEventArgs e)
         {
             points += cookieGains;
-            label_points.Content = "Bananes : " + points;
+            ApplyRandomEvent();
+            updateBananaCount();
 
         }
 
@@ -53,7 +65,7 @@ namespace BlackCookie
             if (points >= cout)
             {
                 points -= cout;
-                label_points.Content = "Bananes : " + points;
+                updateBananaCount();
             }
             else
             {
@@ -96,9 +108,14 @@ namespace BlackCookie
                 // Mettez à jour les données du jeu
                 points = data.CookieCount;
                 cookieGains = data.ClickValue;
-                label_points.Content = "Bananes : " + points;
+                updateBananaCount();
                 // Mettez à jour d'autres propriétés si nécessaire
             }
+        }
+
+        public void updateBananaCount()
+        {
+            label_points.Content = "Bananes : " + points;
         }
 
         private void MainWindow_Closing(object sender, System.ComponentModel.CancelEventArgs e)
@@ -107,6 +124,55 @@ namespace BlackCookie
             SaveGame();
         }
 
+        public void ApplyRandomEvent()
+        {
+            // Générez un nombre aléatoire pour déterminer si un événement aléatoire se produit
+            if (random.NextDouble() < 0.1) // 10% de chance, ajustez selon vos besoins
+            {
+                if (points > 150)
+                {
+                    int randomSubtract = random.Next(1, 100);
+                    points -= randomSubtract;
+                }
+                // Générez un montant aléatoire à soustraire du nombre de cookies
+                
+
+                // Générez un montant aléatoire à soustraire du clickValue
+                if (cookieGains > 15)
+                {
+
+                    int randomSubtractClickValue = random.Next(1, 5); // Par exemple, soustraire entre 1 et 5 au clickValue
+                    cookieGains -= randomSubtractClickValue;
+                }
+
+                // Mettez à jour l'interface utilisateur
+                updateBananaCount();
+            }
+        }
+        private void AutoClickerTimerElapsed(object sender, ElapsedEventArgs e)
+        {
+            uiDispatcher.Invoke(() =>
+            {
+                ApplyAutoClick(); // Appliquer le clic automatique
+                ApplyRandomEvent(); // Appliquer un événement aléatoire, si nécessaire
+            });
+        }
+
+        public void ApplyAutoClick()
+        {
+            uiDispatcher.Invoke(() =>
+            {
+                Click();
+            });
+            
+        }
+        public  void Click()
+        {
+            points += cookieGains;
+            updateBananaCount();
+            ApplyRandomEvent();
+      
+        }
 
     }
 }
